4ec06fbfb80c9b107d65173d136c4a17d9e6f0e9
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-04-03 21:27:55 +0300

    the addEventListeners were added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,46 @@
-const inputEl = document.querySelector('#name-input');
-const outputEl = document.querySelector('#name-output');
+const inputEl = document.querySelector('.text-input');
+const outputEl = document.querySelector('.text-output');
+const buttonEl = document.querySelector('.click-me-button');
 
-inputEl.addEventListener('input', (event) => {
-  outputEl.textContent = event.currentTarget.value;
-  if (event.currentTarget.value === '') {
-    outputEl.textContent = 'enter some text!';
+buttonEl.addEventListener('click', onClick);
+outputEl.addEventListener('mousedown', onMouseDown);
+
+function onClick() {
+  outputEl.textContent = inputEl.value;
+}
+
+function onMouseDown(e) {
+  console.log('event mousedown', e);
+
+  const drag = e.target;
+
+  drag.style.position = 'absolute';
+  drag.style.zIndex = 1000;
+
+  document.body.append(drag);
+
+  function moveDrag(x, y) {
+    drag.style.left = x - drag.offsetWidth / 2 + 'px';
+    drag.style.top = y - drag.offsetHeight / 2 + 'px';
   }
-});
+
+  moveDrag(e.x, e.y);
+
+  function onMouseMove(e) {
+    moveDrag(e.x, e.y);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  // Обробник події 'mouseup' видаляє обробник 'mousemove' після відпускання миші
+
+  drag.onmouseup = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    drag.onmouseup = null;
+  };
+}
+
+// Забороняємо стандартну поведінку перетягування
+drag.ondragstart = function () {
+  return false;
+};
